4a5d6325e462390eb0a03c56292a9e351de180c4
lutfiddindev <lutfiddindev@gmail.com>
2022-07-23 12:22:09 +0500

    made the classes immutable

--- src/com/lutfiddin/test/Main.java
+++ src/com/lutfiddin/test/Main.java
@@ -12,32 +12,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        locations.put(0, new Location(0,"You are at home learning Java"));
-        locations.put(1, new Location(1,"You are at AUT getting your admit card"));
-        locations.put(2, new Location(2,"You are in Tashkent going to ice city"));
-        locations.put(3, new Location(3,"You are at school getting your certificate"));
-        locations.put(4, new Location(4,"You are at gym training to lead a happy lifestyle"));
-        locations.put(5, new Location(5,"You are in the forest"));
+        Map<String, Integer> tempExit = new HashMap<String, Integer>();
+        locations.put(0, new Location(0,"You are at home learning Java",tempExit));
+
 
         // adding exits to each direction 1
-        locations.get(1).addExit("W",2);
-        locations.get(1).addExit("E",3);
-        locations.get(1).addExit("S",4);
-        locations.get(1).addExit("N",5);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W", 2);
+        tempExit.put("E",3);
+        tempExit.put("S",4);
+        tempExit.put("N",5);
+        locations.put(1, new Location(1,"You are at AUT getting your admit card",tempExit));
+
 
         // adding exits to direction 2
-        locations.get(2).addExit("N",5);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N",5);
+        locations.put(2, new Location(2,"You are in Tashkent going to ice city",tempExit));
+
 
         // adding exits to direction 3
-        locations.get(2).addExit("W",1);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("W",1);
+        locations.put(3, new Location(3,"You are at school getting your certificate",tempExit));
+
 
         // adding exits to direction 4
-        locations.get(4).addExit("N",1);
-        locations.get(4).addExit("W",2);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("N",1);
+        tempExit.put("W",2);
+        locations.put(4, new Location(4,"You are at gym training to lead a happy lifestyle",tempExit));
+
 
         // adding exits to direction 5
-        locations.get(5).addExit("S",1);
-        locations.get(5).addExit("W",2);
+        tempExit = new HashMap<String, Integer>();
+        tempExit.put("S",1);
+        tempExit.put("W",2);
+        locations.put(5, new Location(5,"You are in the forest",tempExit));
+
 
         Map<String, String> vocabulary = new HashMap<String, String>();
         vocabulary.put("QUIT","Q");
@@ -45,6 +57,7 @@ public class Main {
         vocabulary.put("SOUTH","S");
         vocabulary.put("WEST","W");
         vocabulary.put("EAST","E");
+
 
         int loc = 1;
         while (true) {
